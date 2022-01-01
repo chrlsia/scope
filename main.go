5234fb1ec359f84cf58ce01d699fb62022349ce8
Chris Siannas <xrsia@otenet.gr>
2022-01-01 21:10:35 +0200

    Step1

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
+var one ="one"
+
 func main(){
-	
+	var somethingElse = "this is a block variable"
+	fmt.Println(somethingElse)
+
+	myFunc()
+}
+
+func myFunc(){
+	fmt.Println(one)
 }
